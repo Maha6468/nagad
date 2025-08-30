30a96664d1aa76e5d088333da511876e5d5bebfb
Maha6468 <motherma7633@gmail.com>
2025-08-31 01:44:13 +0600

    ..

--- lib/cus_Widget/nagod_Logo.dart
+++ lib/cus_Widget/nagod_Logo.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:nagad/cus_Widget/language_Transfer.dart';
 
 class NagodLogo extends StatelessWidget {
   const NagodLogo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Image.asset("assets/images/nagad.png",height: 150,),
-        Text("নগদ",
-          style: TextStyle(
-              fontSize: 50,
-              color: Colors.redAccent,
-              fontWeight: FontWeight.w900),)
-      ],
+    return SizedBox(
+      height: 250,width: 350,
+      child: Stack(
+        alignment: Alignment.center,
+          children: [
+            Positioned(
+              top:-30,
+                child: Image.asset("assets/images/nagad.png",height: 170,)),
+            Positioned(
+              bottom: 55,
+              child: Text("নগদ",
+                style: TextStyle(
+                    fontSize: 70,
+                    color: Colors.red[700],
+                    fontWeight: FontWeight.bold,),),
+            )
+          ],
+      ),
     );
   }
 }
